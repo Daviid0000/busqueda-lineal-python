d12b7a4143b8e73eadeb6d88ad5e3ef4650470ce
David Liva <livadavid78@gmail.com>
2024-05-31 14:21:44 -0300

    Se ha importado una libreria para obtener el tiempo de ejecucion del programa y se ha modificado la manera en la que se muestran los resultados.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import time
+inicio = time.time()
+
 def busqueda_lineal(lista, x):
     i=0
 
@@ -5,10 +8,17 @@ def busqueda_lineal(lista, x):
         if z == x:
             print("Valor obtenido:", z)
             return i
+        print(f"Valor no obtenido: {z}")
         i=i+1
     return -1
 
-lista = [1, 2, 3, 4, 5, -1]
-x = 4
+lista = [1, 6, 5, 4, 5, -1]
+x = 5
 resultado = busqueda_lineal(lista, x)
-print("El valor se encuentra en el indice:", resultado)
+if lista[resultado] == x:
+    print(f"El valor se encuentra en el indice: {resultado}")
+elif lista[resultado] != x:
+    print( "El valor no se encuentra en el lista")
+
+fin = time.time()
+print(f"Tiempo de ejecución: {fin-inicio}")
